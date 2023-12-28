ed64c93f05537a5b391021e8c292d13e544cba81
Tkc2407 <tapaschaubey@gmail.com>
2023-12-28 21:30:10 +0530

    setup gh-pages

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,15 +8,15 @@ const Navbar=()=> {
             <div>
                 <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        {/* <Link className="navbar-brand" to="/">Newsify</Link> */}
-                        <div className="navbar-brand" to="/">Newsify</div>
+                        <Link className="navbar-brand" to="/Newsify">Newsify</Link>
+                        {/* <div className="navbar-brand" to="/">Newsify</div> */}
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                    <Link className="nav-link" aria-current="page" to="/Newsify">Home</Link>
                                 </li>
                                 <li className="nav-item">
                                     <Link className="nav-link" to="/business">Business</Link>
