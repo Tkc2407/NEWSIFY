a02717e7e58460cdebbeed31d0f93a38ce342616
Tkc2407 <tapaschaubey@gmail.com>
2023-12-28 21:09:06 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App=()=> {
             progress={progress}
           />
           <Routes>
-            <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={20} country="in" category="general" />} />
+            <Route exact path="/Newsify" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={20} country="in" category="general" />} />
             <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={5} country="in" category="business" />} />
             <Route exact path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={5} country="in" category="entertainment" />} />
             <Route exact path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={5} country="in" category="general" />} />
